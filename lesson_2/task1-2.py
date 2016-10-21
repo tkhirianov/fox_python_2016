62869ef7c7a3b8aac5395ea5d7959b2770ce6fa1
Timofey Khirianov <tkhirianov@gmail.com>
2016-10-21 17:38:43 +0300

    Уроки 2 и 3

---
+++ lesson_2/task1-2.py
@@ -0,0 +1,45 @@
+#-*- coding: utf-8 -*
+import robot
+r = robot.rmap()
+r.loadmap('task1-2')
+def task():
+    pass
+    #------- пишите код здесь -----
+    r.right()
+    r.right()
+    r.down()
+    r.down()
+    r.paint()
+    r.right()
+    r.right()
+    r.down()
+    #------- пишите код здесь -----
+r.start(task)
+
+#Отступ слева (tab) сохранять!
+#r.help() - Список команд и краткие примеры
+#r.demo() - показать решение этой задачи (только результат, не текст программы)
+#r.demoAll() - показать все задачи (примерно 20 минут)
+
+#r.right() - вправо
+#r.down() - вниз
+#r.up() - вверх
+#r.left() - влево
+#r.paint() - закрасить  Paint
+
+#r.color() - закрашена ли клетка? Color
+#r.freeRight() - свободно ли справа? freeRight
+#r.freeLeft() - свободно ли слева?  freeLeft
+#r.freeUp() - свободно ли сверху? freeUp
+#r.freeDown() - свободно ли снизу?  freeDown
+
+#r.wallRight() - стена ли справа? wallRight
+#r.wallLeft() - стена ли слева?  wallLeft
+#r.wallUp() - стена ли сверху? wallUp
+#r.wallDown() - стена ли снизу?  wallDown
+
+
+#red - красный
+#blue - синий
+#yellow - желтый
+#green - зеленый
---
+++ lesson_3/task5_5.py
@@ -0,0 +1,50 @@
+#-*- coding: utf-8 -*
+import robot
+r = robot.rmap()
+r.loadmap('task5-5')
+def task():
+    pass
+    #------- пишите код здесь -----
+
+    # до упора вниз
+    while r.freeDown():
+        r.down()    
+    # вправо до конца стены
+    while r.wallDown():
+        r.right()
+    # вниз
+    r.down()
+    # до упора влево
+    while r.freeLeft():
+        r.left()
+        
+    #------- пишите код здесь -----
+r.start(task)
+
+#Отступ слева (tab) сохранять!
+#r.help() - Список команд и краткие примеры
+#r.demo() - показать решение этой задачи (только результат, не текст программы)
+#r.demoAll() - показать все задачи (примерно 20 минут)
+
+#r.right() - вправо
+#r.down() - вниз
+#r.up() - вверх
+#r.left() - влево
+#r.paint() - закрасить  Paint
+
+#r.color() - закрашена ли клетка? Color
+#r.freeRight() - свободно ли справа? freeRight
+#r.freeLeft() - свободно ли слева?  freeLeft
+#r.freeUp() - свободно ли сверху? freeUp
+#r.freeDown() - свободно ли снизу?  freeDown
+
+#r.wallRight() - стена ли справа? wallRight
+#r.wallLeft() - стена ли слева?  wallLeft
+#r.wallUp() - стена ли сверху? wallUp
+#r.wallDown() - стена ли снизу?  wallDown
+
+
+#red - красный
+#blue - синий
+#yellow - желтый
+#green - зеленый
