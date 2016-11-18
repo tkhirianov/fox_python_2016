0db8aa2be6eed4fb652a6cb3912dc9804bdc4761
Timofey Khirianov <tkhirianov@gmail.com>
2016-11-18 17:19:30 +0300

    2-3 задачи

--- lesson_7/02_pentagon.py
+++ lesson_7/02_pentagon.py
@@ -1,9 +1,41 @@
 from turtle import *
-shape('turtle')
-shapesize(2)
-color('black', 'darkgreen')
-width(3)
 
-for side in range(4):
-    forward(200)
-    left(90)
+def init():
+    shape('turtle')
+    shapesize(2)
+    color('black', 'darkgreen')
+    width(3)
+    penup()
+    backward(200)
+
+def pentagon(edge_length):
+    """ перо поднято по окончании
+        рисования пятиугольника
+        edge_length - длина ребра
+    """
+    pendown()
+    begin_fill()
+    for side in range(5):
+        forward(edge_length)
+        left(360/5)
+    end_fill()
+    penup()
+
+def star(length):
+    """ перо поднято по окончании
+        рисования пятиугольника
+        length - длина ребра
+    """
+    pendown()
+    begin_fill()
+    for side in range(5):
+        forward(length)
+        right(2*360/5)
+    end_fill()
+    penup()
+        
+init()
+star(150)
+forward(200)
+pentagon(70)
+hideturtle()
---
+++ lesson_7/03_mnogougolniki.py
@@ -0,0 +1,51 @@
+from turtle import *
+
+def init():
+    shape('turtle')
+    shapesize(2)
+    color('black', 'darkgreen')
+    width(3)
+    penup()
+    backward(300)
+
+def polygon(edges_number, edge_length):
+    """ перо поднято по окончании
+        рисования многоугольника
+        edges_number - количество рёбер (углов)
+        edge_length - длина ребра
+    """
+    pendown()
+    begin_fill()
+    for side in range(edges_number):
+        forward(edge_length)
+        left(360/edges_number)
+    end_fill()
+    penup()
+
+def star(rays_number, length):
+    """ перо поднято по окончании рисования звезды
+        length - длина ребра
+        rays_number - количество лучей (нечётное!)      
+    """
+    pendown()
+    begin_fill()
+    for side in range(rays_number):
+        forward(length)
+        right((rays_number//2)*360/rays_number)
+    end_fill()
+    penup()    
+        
+init()
+for n in range(3, 10):
+    polygon(n, 50)
+    forward(100)
+    
+backward(7*100)
+right(90)
+forward(150)
+left(90)
+
+for n in range(3, 16, 2):
+    star(n, 80)
+    forward(100)
+    
