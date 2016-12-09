afd05f63d0b061d5d3d308119c326fa07e6daa3e
Timofey Khirianov <tkhirianov@gmail.com>
2016-12-09 17:24:15 +0300

    ГРИС Чертёжник - бета-версия

---
+++ lesson_10/exercise1.py
@@ -0,0 +1,16 @@
+from drawman import *
+
+colours = ['red', 'green', 'blue']*5
+back_colours = ['red', 'green', 'blue']*5
+
+init_drawman()
+for i in range(10, 0, -1):
+    pendown()
+    color(colours[i], back_colours[i])
+    begin_fill()
+    on_vector(i, 0)
+    on_vector(0, i)
+    on_vector(-i, 0)
+    on_vector(0, -i)
+    end_fill()
+    penup()
