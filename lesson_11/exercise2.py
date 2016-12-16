9477a7e49016a9247c0e23d1412cd4ab4076a6f8
Timofey Khirianov <tkhirianov@gmail.com>
2016-12-16 17:41:31 +0300

    Тексты урока 11. Итоговые.

--- lesson_11/exercise2.py
+++ lesson_11/exercise2.py
@@ -3,24 +3,29 @@ from drawman import *
 
 def main():
     init_drawman()
-    A = [(1, 1), (3, 0), (6, 2), (8, -5)]
+    A = [(0, 0), (1, 1), (3, 0), (6, 2), (7, 0)]
     draw_curve(A)
-
+    color('green', 'yellow')
+    fill_polygon(A)
+    B = [(x, -y) for x, y in A]
+    fill_polygon(B)
 
 
 def draw_curve(A):
     penup()
-    to_point(A[0][0], A[0][1])
+    to_point(*A[0])
     pendown()
-    for point in A:
-        x, y = point
+    for x, y in A:  # NB! Пробегаем кортежем переменных по списку кортежей!
         to_point(x, y)
     penup()
 
 def fill_polygon(A):
     begin_fill()
     draw_curve(A)
-    to_point(A[0][0], A[0][1])
+    pendown()
+    to_point(*A[0])
+    penup()
     end_fill()
+    
 
 main()
