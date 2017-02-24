f3024364d04f17eb5e492679f0eac862b1402eb4
Timofey Khirianov <tkhirianov@gmail.com>
2017-02-24 17:40:08 +0300

    Картинга png в качестве шарика и линия-пушка.

--- lesson_19/canvas_usage.py
+++ lesson_19/canvas_usage.py
@@ -1,5 +1,6 @@
 from tkinter import *
 dt = 50
+ball_radius = 10
 
 
 class MainWindow:
@@ -9,9 +10,12 @@ class MainWindow:
         self.canvas.pack()
         self.a = [300, 200]
         self.line = self.canvas.create_line(self.a, [400, 200], width=5, fill='red')
-        self.ball = self.canvas.create_oval(0, 0, 40, 40)
+        self.balls = []
+        self.balls_velocity = []
+        self.ball_sprite = PhotoImage(file="ball_sprite.png")
 
         self.canvas.bind('<Motion>', self.mouse_motion)
+        self.canvas.bind('<Button-1>', self.mouse_click)
         self.canvas.after(dt, self.game_cycle)
         self.root.mainloop()
 
@@ -19,10 +23,20 @@ class MainWindow:
         b = [event.x, event.y]
         self.canvas.coords(self.line, *self.a, *b)
 
-    def game_cycle(self, *ignore):
-        print('Очередная итерация игры')
-        self.canvas.move(self.ball, 1, 1)
+    def mouse_click(self, event):
+        #ball = self.canvas.create_oval(event.x - ball_radius, event.y - ball_radius,
+        #                               event.x + ball_radius, event.y + ball_radius)
+        ball = self.canvas.create_image(event.x - ball_radius, event.y - ball_radius,
+                                        image=self.ball_sprite, tag='ball')
+        vx = round((event.x - self.a[0])/10)
+        vy = round((event.y - self.a[1])/10)
+        self.balls_velocity.append([vx, vy])
+        self.balls.append(ball)
 
+    def game_cycle(self, *ignore):
         self.canvas.after(dt, self.game_cycle)  # перезапуск цикла
+        for ball, velocity in zip(self.balls, self.balls_velocity):
+            self.canvas.move(ball, velocity[0], velocity[1])
+
 
 window = MainWindow()
