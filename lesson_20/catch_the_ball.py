f7131df5f59870ed1875369a36f490e008fff0d7
Timofey Khirianov <tkhirianov@gmail.com>
2017-03-03 17:14:03 +0300

    Игра "Поймай шарик!". Заготовка 2. Запускается.

--- lesson_20/catch_the_ball.py
+++ lesson_20/catch_the_ball.py
@@ -6,24 +6,23 @@ screen_height = 400
 r_min = 10
 r_max = 30
 dt = 10  # микросекунд
+ball_sprite_filename = "ball_sprite.png"
 
 
 class MainWindow:
-    def __init__(self):
-        self.root = Tk()
-
-        self.canvas = Canvas(root)
-        self.canvas["width"] = screen_width
-        self.canvas["height"] = screen_height
-        self.canvas.pack()
+    def __init__(self, root):
+        global canvas
+        canvas = Canvas(root)
+        canvas["width"] = screen_width
+        canvas["height"] = screen_height
+        canvas.pack()
 
         self.ball = None
         self.scores = 0
         self.init_game()
         self.game_cycle()  # запуск игрового цикла
 
-        self.canvas.bind("<Button-1>", self.mouse_click)
-        self.root.mainloop()
+        canvas.bind("<Button-1>", self.mouse_click)
 
     def init_game(self):
         x = randint(0, screen_width-1)
@@ -31,10 +30,24 @@ class MainWindow:
         r = randint(r_min, r_max)
         self.ball = Ball(x, y, r)
 
+    def mouse_click(self, event):
+        """ Проверяем, далеко ли шарик, и, если в него попали, то "лопаем" его,
+            создаём новый шарик, а за старый начисляем очки.
+        """
+        pass  # FIXME
+
     def game_cycle(self, *ignore):
-        self.canvas.after(dt, self.game_cycle)  # перезапуск цикла
+        canvas.after(dt, self.game_cycle)  # перезапуск цикла
         # FIXME: что-то делаем с шариком
+
 
 class Ball:
     def __init__(self, x, y, r):
+        self.x, self.y, self.r = x, y, r
+        self.avatar = canvas.create_image(x - r, y - r, image=ball_sprite)
         pass  #FIXME!
+
+root_window = Tk()
+ball_sprite = PhotoImage(file=ball_sprite_filename)
+window = MainWindow(root_window)
+root_window.mainloop()
